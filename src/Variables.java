6c240863049378b9b6f893c7ab49eb199887031f
Iron-Cow <uangeji@gmail.com>
2022-02-19 17:05:14 +0200

    Variables and primitives

---
+++ src/Variables.java
@@ -0,0 +1,26 @@
+public class Variables {
+    public static void main(String[] args) {
+
+        int myFirstInt; // variable declaration
+        myFirstInt = 557;
+        // int myFirstInt = 557; // one line declaration + value assign
+        System.out.println(myFirstInt);
+
+        // INTEGERS
+        int myInt; // variable declaration
+        myInt = 557;
+        short myShort = 3266; // short type limited to 16-bit (−32 768 to 32 767) integers
+        long myLong = 12341234; // long type limited to 64-bit integers;
+
+        // FLOATS
+        double myDouble = 235.33; // 64 bit
+        float myFloat = 235.33f; // 32 bit
+
+        // Others
+        boolean isGood = true;
+        char c = 'a'; // single quotes
+        byte myByte = 100; // -128 to 187
+
+
+    }
+}
