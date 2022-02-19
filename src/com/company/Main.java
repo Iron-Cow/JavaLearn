783e02110167524108ade5194304e0cda7bb8840
Iron-Cow <uangeji@gmail.com>
2022-02-19 16:49:09 +0200

    Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,10 @@
+// To run code - type command below or use play ->  ▶︎ Main in the IDE
+// java src/com/company/Main.java
 package com.company;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    System.out.println("Hello world!");
     }
 }
