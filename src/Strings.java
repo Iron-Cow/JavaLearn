ca0ffc306fbd49b07b8668a3c85006f257b5580c
Iron-Cow <uangeji@gmail.com>
2022-02-19 17:52:14 +0200

    Strings

---
+++ src/Strings.java
@@ -0,0 +1,14 @@
+public class Strings {
+    public static void main(String[] args) {
+        int x = 5;
+
+        String s = "Hello"; // Capital letter in String - convention to name of class (lower case for primitives)
+        // s is link to string object "Hello"
+
+        String space = " ";
+        String name = "Bob";
+        System.out.println(s + space + name);
+        System.out.println("Hello" + " " + "John");
+        System.out.println("My number is " + x);
+    }
+}
