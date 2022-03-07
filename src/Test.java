f90aa9db5ff377d9c3ebd39abe891ba3f28fed50
Iron-Cow <uangeji@gmail.com>
2022-03-07 14:01:57 +0200

    primitive types conversions

---
+++ src/Test.java
@@ -0,0 +1,45 @@
+public class Test {
+    public static void main(String[] args) {
+//        int x = 23; // 32 bit
+//        byte b = 123; // -128 to 127
+//        long y = 123; // 64 bit
+//        double d = 12.3; //64 bit
+//        float f = 12.2f; //32 bit
+
+//        float f = 123.2; // error (double by default)
+//        float f = 123.2f; // ok
+//        float f = (float)123.3; // correct transformation
+//        long l = 12312323123; // error, too  long (int by default)
+//        long l = 12312323123L; // ok
+
+//        int a = 123;
+//        long l = a; // ok // explicit transformation
+//        int x = l; // error // implicit transformation
+//        int x = (int)l; // ok // correct transformation
+//        System.out.println(x);
+
+
+//        int a = 123;
+//        double b = a; // ok
+//        System.out.println(b); // 123.0
+
+//        double x = 123.5;
+////        int y = x; // error // explicit not possible for double -> int
+//        int y = (int)x; // ok
+//        System.out.println(y); // 123 (no decimal part)
+//        long l = Math.round(x);
+//        System.out.println(l); // 124
+
+////        byte x = 128; // error
+//        byte x = (byte)128;
+//        System.out.println(x); // -128 // 128 = 127 + 1// SO 125, 126, 127, -128, -127, -126 ...
+//        byte y = (byte)130; // 127 + 3
+//        System.out.println(y); // -126
+
+
+//        int a = 222222;
+//        a *= 10000;
+//        System.out.println(a); //-2072747296
+
+    }
+}
