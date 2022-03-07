1aa0ea1f8620326136e046ca79a4bc06e11ddc50
Iron-Cow <uangeji@gmail.com>
2022-03-07 16:05:09 +0200

    primitive types wrappers

---
+++ src/Test.java
@@ -0,0 +1,25 @@
+public class Test {
+    public static void main(String[] args) {
+        int xx = 1;
+//        xx. // no options, methods available
+
+
+        // wrapper classes
+        // Double, Float, Long, Integer, Short, Byte, Character, Boolean
+        Integer x = 123; // primitive packing
+//        Integer x = new Integer(130); // same but deprecated
+//        x. // methods available
+        Integer.parseInt("123");
+        System.out.println(x.byteValue()); // -126
+        int y = x; // primitive unpacking
+
+
+        // -------
+
+//        Boolean.TRUE, Boolean.FALSE; // constants
+
+        //
+
+
+    }
+}
