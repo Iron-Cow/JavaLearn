b0c205d5fcbdd1e464f8cdc015d435d5bffc4d9b
Iron-Cow <uangeji@gmail.com>
2022-03-08 21:28:44 +0200

    transient / uid / try with resources

--- src/Person.java
+++ src/Person.java
@@ -1,8 +1,15 @@
+import java.io.Serial;
 import java.io.Serializable;
 
 public class Person implements Serializable {
-    private int id;
-    private String name;
+    @Serial
+    private static final long serialVersionUID = -3982538843988778425L;
+
+    // generate after any changes in the class to keep serializable objects compatible
+
+
+    private /*transient*/ int id;  // primitive transient fields will be converted to 0
+    private /*transient*/ String name; // test transient field (will not be serialized) / null will be displayed
 
     public Person(int id, String name){
         this.id = id;
--- src/ReadObject.java
+++ src/ReadObject.java
@@ -5,17 +5,14 @@ import java.util.Arrays;
 public class ReadObject {
     public static void main(String[] args) {
         // create file people.bin in root dir (same level as src/)
-        try {
-            FileInputStream fis = new FileInputStream("people.bin"); // any extension
-            ObjectInputStream ois = new ObjectInputStream(fis);
-
+        try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream("people.bin"))){
+            // refactored/ no need to close now (try with resources)
             int personCount = ois.readInt();
             Person[] people = new Person[personCount];
 //            Person[] people = (Person[]) ois.readObject(); // case 2 read Array as an object ...
             for (int i = 0; i < personCount; i++){
                 people[i] = (Person) ois.readObject();
             }
-            ois.close();
             System.out.println(Arrays.toString(people));
             for (Person p: people){
                 System.out.println(p);
