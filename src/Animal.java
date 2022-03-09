c52d89f42192a18442d422ea2b4723f9dd8fb899
Iron-Cow <uangeji@gmail.com>
2022-03-09 16:26:23 +0200

    abstract class demo

---
+++ src/Animal.java
@@ -0,0 +1,13 @@
+public abstract class Animal {
+    public void eat(){
+        System.out.println("I am eating");
+    }
+
+    public abstract void makeSound(); // abstract method required to implement in child classes
+}
+
+// Interface - contract.
+// Abstract class - the thing that class is
+
+
+
---
+++ src/Test.java
@@ -0,0 +1,10 @@
+public class Test {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        Dog dog = new Dog();
+//        Animal animal = new Animal(); // no need to create Animal instance (as it is abstract)
+        // + we cannot create instance of abstract class
+
+
+    }
+}
