0babc7a245aaad821397ae97257e6d19972ee188
Iron-Cow <uangeji@gmail.com>
2022-03-21 11:07:55 +0200

    junit annotations demo

--- src/test/java/Vector2DTest.java
+++ src/test/java/Vector2DTest.java
@@ -1,28 +1,40 @@
 import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class Vector2DTest {
+    // add constants as final to avoid magic numbers
+    private final double EPS = 1e-9;
+
+    //----- move vector creation to helping method
+    private static Vector2D v1;
+
+    @Before // run before each @Test method
+    public void createNewVector(){
+        v1 = new Vector2D();
+    }
+
+    // OR
+
+    @BeforeClass // run once on class creation
+    public static void createNewVectorOnce(){
+        v1 = new Vector2D();
+    }
+    //-----
+
     @Test
     public void newVectorShouldHaveZeroLength(){
-        Vector2D v1 = new Vector2D(); // action
-
-        // assertion
-                            // expected, actual, possible discrepancy of results
-                                                    // 1e-9 = 0.000000001
-        Assert.assertEquals(0, v1.length(), 1e-9);
+        Assert.assertEquals(0, v1.length(), EPS);
     }
 
     @Test
-    public void newVectorShouldHaveZeroX(){
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getX(), 1e-9);
+    public void newVectorShouldHaveZeroXY(){
+        // multiple asserts can be stored in one test case
+        Assert.assertEquals(0, v1.getX(), EPS);
+        Assert.assertEquals(0, v1.getY(), EPS);
     }
 
-    @Test
-    public void newVectorShouldHaveZeroY(){
-        Vector2D v1 = new Vector2D();
-        Assert.assertEquals(0, v1.getY(), 1e-9);
-    }
 
 }
 
